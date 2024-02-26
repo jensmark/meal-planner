8df4e8006f308c98526db8cf2193bc36a0da8109
Jens Kristoffer Reitan Markussen <jens.markussen@knowit.no>
2024-02-26 21:37:20 +0100

    update model

--- app/recipe/page.tsx
+++ app/recipe/page.tsx
@@ -25,7 +25,6 @@ export default async function Recipe() {
             <p>Julie sin fantastiske middagsplanlegger</p>
           </div>
         </main>
-  
       </div>
     )
   }
--- lib/drizzle.ts
+++ lib/drizzle.ts
@@ -4,7 +4,8 @@ import {
   text,
   timestamp,
   real,
-  integer
+  integer,
+  varchar
 } from 'drizzle-orm/pg-core'
 import { InferSelectModel, InferInsertModel } from 'drizzle-orm'
 import { sql } from '@vercel/postgres'
@@ -26,30 +27,54 @@ export const RecipeTable = pgTable(
   'recipe', 
   {
     id: serial('id').primaryKey(),
-    createdBy: integer('created_by'),
-    name: text('name').notNull().unique(),
+    createdBy: text('created_by'),
+    name: text('name').notNull(),
     createdAt: timestamp('created_at').defaultNow().notNull(),
-    servings: integer('servings').default(1).notNull()
+    servings: real('servings').default(1.0).notNull()
   }
 )
 export type Recipe = InferSelectModel<typeof RecipeTable>
 export type NewRecipe = InferInsertModel<typeof RecipeTable>
 
 
-export const AppliedIngredientTable = pgTable(
-  'applied_ingredient', 
+export const RecipeIngredientTable = pgTable(
+  'recipe_ingredient', 
   {
     id: serial('id').primaryKey(),
     ingredientId: integer('ingredient_id').references(() => IngredientTable.id),
     recipeId: integer('recipe_id').references(() => RecipeTable.id),
-    quantity: real('quantity')
+    quantity: real('quantity').default(1.0).notNull(),
+    measurementUnit: varchar('measurement_unit').default('count').notNull()
   }
 )
-export type AppliedIngredient = InferSelectModel<typeof AppliedIngredientTable>
-export type NewAppliedIngredient = InferInsertModel<typeof AppliedIngredientTable>
+export type RecipeIngredient = InferSelectModel<typeof RecipeIngredientTable>
+export type NewRecipeIngredient = InferInsertModel<typeof RecipeIngredientTable>
 
+export const MealWeekTable = pgTable(
+  'meal_week',
+  {
+    id: serial('id').primaryKey(),
+    year: integer('year').notNull(),
+    weekNumber: integer('week_number').notNull(),
+    ownerId: text('owner_id').notNull()
+  }
+)
 
+export type MealWeek = InferSelectModel<typeof MealWeekTable>
+export type NewMealWeek = InferInsertModel<typeof MealWeekTable>
 
+export const WeekDayMealTable = pgTable(
+  'week_day_meal',
+  {
+    id: serial('id').primaryKey(),
+    mealWeekId: integer('meal_week_id').references(() => MealWeekTable.id),
+    recipeId: integer('recipe_id').references(() => RecipeTable.id),
+    dayOfWeek: integer('day_of_week').notNull()
+  }
+)
+
+export type WeekDayMeal = InferSelectModel<typeof WeekDayMealTable>
+export type NewWeekDayMeal = InferInsertModel<typeof WeekDayMealTable>
 
 // Connect to Vercel Postgres
 export const db = drizzle(sql)
